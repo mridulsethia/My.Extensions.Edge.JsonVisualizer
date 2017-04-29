f379f5e0209be6c1c4d8fe59a3b2a479b9689f34
Mridul Sethia <mridulsethia@hotmail.com>
2017-04-29 11:32:06 +0200

    added the json visualizer extension and edge extension related files

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# My.Extensions.Edge.JsonVisualizer
-This extension renders a JSON response in a more reader friendly layout 
+
+# Edge Extension: JSON Visualizer
+
+This Edge browser extension converts a JSON response into an object tree to provide a better reading experience. It also allows the user to expand/collapse objects in the tree.
+
+
+## Steps to create a package to submit the extension in the Windows Store
+
+Once you are finished with the development of your app, you would need to go through the following steps before it can be listed in the Windows Store.
+
+
+### Step 1: Developer account and the preparation for app submission
+
+- If you do not have a windows developer account, create one by paying the nominal fee to list your app(s) in the Windows Store
+- Login to your developer account and create a new app, reserve a catchy name for your upcoming app
+- Go to "App management" and check the app identity details (it will be required when creating the package)
+
+
+### Step 2: Use manifoldjs to create package folder structure 
+
+- Install Nodejs and node_module manifoldjs (npm -install g manifoldjs) 
+- Start Node.js command prompt, swtich to the folder where you want to copy the package artifacts (e.g. run "cd C:\EdgeExtensions")
+- Run command: manifoldjs -l debug -p edgeextension -f edgeextension -m C:\Git\My.Extensions.Edge.JsonVisualizer\manifest.json
+- Verify folder structure and that all the required files are correctly copied under the edgeextension\manifest\Extension folder
+- Copy correct icons in the Assets folder
+- Edit the appxmanifest.xml with correct app identity details (we saw it in the Step 1)
+
+
+### Step 3: Use MakeAppx tool to create the package 
+
+- Make sure that you have Win SDK installed, verify that you have (C:\Program Files (x86)\Windows Kits\10\bin\x64) folder
+- Start cmd as administrator 
+- Run command: "C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /h SHA256 /d "C:\EdgeExtensions\JSONVisualizer\edgeextension\manifest" /p C:\EdgeExtensions\AppexTest\JSONVisualizer.appx
+- Verify that an appx file is created in the destination folder
+
+
+### Step 4: Verify your package before submitting
+
+- Start cmd as administrator
+- Unpack the newly created package by running the command: "C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" unpack /v /p C:\EdgeExtensions\AppexTest\JSONVisualizer.appx /d C:\EdgeExtensions\AppexTest\JSONVisualizer
+- Sideload the extension in the Edge browser and check everything works as expected
+
+
+### Step 5: The package submission and the approval
+
+- Log on to your developer account and upload the appex package to the correct App (if you have more than one) 
+- Choose desired Pricing, Properties, Age rating and Store listing
+- Provide some info in "Notes for certification" for testers to understand what your app does
+- Wait for the approval from Microsoft
+
+
+## Other relevant info
+
+- Info on how to create an app package signing certificate: https://msdn.microsoft.com/en-us/library/windows/desktop/jj835832.aspx
---
+++ js/content.js
@@ -0,0 +1,105 @@
+!function($) {
+	'use strict';
+
+	$.fn.jsonVisualizer = function(json, options) {
+		return this.each( function() {
+			var self = $(this);
+			if (typeof json == 'string') {
+				self.data('json', json);
+			}
+			else if(typeof json == 'object') {
+				self.data('json', JSON.stringify(json))
+			}
+			else {
+				self.data('json', '');
+			}
+
+			try {
+				var jObject = $.parseJSON(json); // only if json response, we go ahead with formatting
+				self.data('json', jObject);
+			} catch(error) {
+				return; // its not json
+			}
+
+			new JsonVisualizer(self, options);
+		});
+	};
+
+	function JsonVisualizer(self, options) {
+		self.html('<ul class="jv-container"></ul>');
+		options = $.extend({}, this.defaults, options);
+		var renderModes = getRenderingModes(options.expanded);
+		self.find('.jv-container').append(renderAsHtml([self.data('json')], renderModes));
+	}
+
+	function renderAsHtml(json, renderModes, isArray = false) {
+		var html = '';
+		for(var key in json) {
+			if (!json.hasOwnProperty(key)) {
+				continue;
+			}
+
+			var value = json[key], type = typeof json[key];
+			html += createElement(key, value, type, renderModes, isArray);
+		}
+		return html;
+	}
+
+	function createElement(key, value, type, renderModes, isArray) {
+		var cssClass = 'object',
+        	open = '{',
+        	close = '}';
+		if ($.isArray(value)) {
+			cssClass = 'array';
+      		open = '[';
+      		close = ']';
+		}
+		if(value === null) {
+			return '<li><span class="key">' + encode(key) + ': </span><span class="null">' + encode(value) + '</span></li>';
+		}
+		if(type == 'object') {
+			var object = '<li><span class="'+ renderModes +'"></span>';
+			object += ('<span class="key">' + ((isArray) ? '' : (encode(key) + ':')) + ' </span>');
+			object += (' <span class="open">' + open + '</span> <ul class="' + cssClass + '">');
+			object += renderAsHtml(value, renderModes, $.isArray(value));
+			return object += ('</ul><span class="close">' + close + '</span></li>');
+		}
+		if(type == 'number' || type == 'boolean') {
+			return '<li><span class="key">' + encode(key) + ': </span><span class="'+ type + '">' + encode(value) + '</span></li>';
+		}
+		return '<li><span class="key">' + encode(key) + ': </span><span class="'+ type + '">"' + encode(value) + '"</span></li>';
+	}
+
+	function getRenderingModes(expanded) {
+		if(!expanded) return 'expanded collapsed hidden';
+		return 'expanded';
+	}
+
+	function encode(value) {
+		return $('<div/>').text(value).html();
+	}
+
+	$(document).on('click', '.jv-container .expanded', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var $self = $(this);
+		$self.parent().find('>ul').slideUp(100, function() {
+			$self.addClass('collapsed');
+		});
+	});
+
+	$(document).on('click', '.jv-container .expanded.collapsed', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var $self = $(this);
+		$self.removeClass('collapsed').parent().find('>ul').slideDown(100, function() {
+			$self.removeClass('collapsed').removeClass('hidden');
+		});
+	});
+
+	JsonVisualizer.prototype.defaults = {
+		expanded: true
+	};
+
+}(window.jQuery);
+$('body').jsonVisualizer(document.body.textContent);
